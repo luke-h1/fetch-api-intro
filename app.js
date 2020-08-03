c1d0474360ac533f0311bc2f2bc33dff90771c05
luke-h1 <luke.howsam@yahoo.com>
2020-08-03 15:27:11 +0100

    tutorial finish

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-document.getElementById('getText').addEventListener('click', getText); 
-
+document.getElementById('getText').addEventListener('click', getText);
 document.getElementById('getUsers').addEventListener('click', getUsers);
-
+document.getElementById('getPosts').addEventListener('click', getPosts);
+document.getElementById('addPost').addEventListener('submit', addPost);
 // normal functions
 // function getText() {
 //     fetch('sample.txt').then(function (res) {
@@ -13,7 +13,6 @@ document.getElementById('getUsers').addEventListener('click', getUsers);
 // }
 
 // arrow functions:
-
 function getText() {
     fetch('sample.txt')
         .then((res) => res.text())
@@ -26,21 +25,51 @@ function getText() {
 function getUsers() {
     fetch('users.json').then((res) =>
         res.json().then((data) => {
-            let output = '<h2>Users</h2>';
+            let output = '<h2 class="mb-4">Users</h2>';
             data.forEach(function (user) {
                 output += `
-              <ul> 
-              <li>ID: ${user.id}</li>  
-              <li>Name: ${user.name}</li> 
-              <li>Email: ${user.email}</li> 
+              <ul class="list-group mb-3"> 
+              <li class="list-group-item">ID: ${user.id}</li>  
+              <li class="list-group-item">Name: ${user.name}</li> 
+              <li class="list-group-item">Email: ${user.email}</li> 
               </ul> 
-              
               `;
-            }); 
+            });
 
             document.getElementById('output').innerHTML = output;
         })
     );
 }
 
-// getUsers()
+function getPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((res) => res.json())
+        .then((data) => {
+            let output = `<h2 mb-4>posts</h2>`;
+            data.forEach(function (post) {
+                output += ` 
+                <div class="card card-body mb-3"> 
+                <h3>${post.title}</h3>  
+                <p>${post.body}</p>  
+                </div>`;
+            });
+            document.getElementById('output').innerHTML = output;
+        });
+}
+
+function addPost(e) {
+    e.preventDefault();
+    let title = document.getElementById('title').value;
+    let body = document.getElementById('body').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ title: title, body: body }),
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+}
